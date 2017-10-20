fa5f7463387b867c05dc040ef3d7c51a46bc67dc
prathv <prathveer.r@gmail.com>
2017-10-20 13:30:08 -0700

    Added RollerBall Windows build and controller project sln'

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerController : MonoBehaviour {
+    public Text countText;
+    public Rigidbody rb;
+    private int count ;
+    public float speed;
+    public Text winText;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        winText.text = "";
+        setCountText();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Pick UP"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            setCountText();
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 6)
+        {
+            winText.text = "Winner!";
+        }
+    }
+}
